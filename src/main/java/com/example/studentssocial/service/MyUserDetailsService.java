f7a15d079e3972631a8633a57d62a48450acf61f
cosminlucian48 <lucian.bejan-topse@s.unibuc.ro>
2021-12-04 15:41:26 +0200

    disabled jwt and spring security

--- src/main/java/com/example/studentssocial/service/MyUserDetailsService.java
+++ src/main/java/com/example/studentssocial/service/MyUserDetailsService.java
@@ -1,19 +1,19 @@
-package com.example.studentssocial.service;
-
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.stereotype.Service;
-
-import java.util.ArrayList;
-
-@Service
-public class MyUserDetailsService implements UserDetailsService {
-    @Override
-    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-//        Ar trebui de facut un model User
-        return new User("admin","admin",new ArrayList<>());
-
-    }
-}
+//package com.example.studentssocial.service;
+//
+//import org.springframework.security.core.userdetails.User;
+//import org.springframework.security.core.userdetails.UserDetails;
+//import org.springframework.security.core.userdetails.UserDetailsService;
+//import org.springframework.security.core.userdetails.UsernameNotFoundException;
+//import org.springframework.stereotype.Service;
+//
+//import java.util.ArrayList;
+//
+//@Service
+//public class MyUserDetailsService implements UserDetailsService {
+//    @Override
+//    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+////        Ar trebui de facut un model User
+//        return new User("admin","admin",new ArrayList<>());
+//
+//    }
+//}
