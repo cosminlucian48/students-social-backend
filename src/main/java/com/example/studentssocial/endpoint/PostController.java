9c97da1a181de5691e09811f5dbe2bd7fec5c271
Danielsan222 <drobnitchidaniel@users.noreply.github.com>
2021-12-13 17:49:22 +0200

    Delete+GetPost Done, Put not done

--- src/main/java/com/example/studentssocial/endpoint/PostController.java
+++ src/main/java/com/example/studentssocial/endpoint/PostController.java
@@ -1,19 +1,23 @@
 package com.example.studentssocial.endpoint;
 
-
 import com.example.studentssocial.dto.PostDto;
 import com.example.studentssocial.entity.Post;
-import com.example.studentssocial.entity.User;
 import com.example.studentssocial.service.PostService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.*;
+import com.example.studentssocial.mapper.PostMapper;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping(value = "/post")
 public class PostController {
 
+
+    private Logger logger = LoggerFactory.getLogger(SubjectController.class);
     private final PostService postService;
 
     @Autowired
@@ -22,6 +26,20 @@ public class PostController {
     @GetMapping
     public List<Post> getAllPosts(){return postService.getAllPosts();}
 
+    @GetMapping(value = "/{id}")
+    public Optional<Post> getSubject(@PathVariable("id") Long id) { return postService.getPost(id); }
+
+    @DeleteMapping("/{id}")
+    public void deleteSubject(@PathVariable Long id) {
+        logger.info("Deleted subject with {}", id);
+        postService.deletePost(id);
+    }
+    @PutMapping("/{id}")
+        public Post updatePost(@PathVariable Long id, @RequestBody Post post) {
+        postService.updatePost(id, post);
+        logger.info("Updated Post {}", post);
+        return post;
+    }
     @PostMapping
     public PostDto postPost(@RequestBody PostDto postDto){return postService.savePost(postDto);}
 }
--- src/main/java/com/example/studentssocial/service/PostService.java
+++ src/main/java/com/example/studentssocial/service/PostService.java
@@ -9,6 +9,7 @@ import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class PostService {
@@ -16,24 +17,44 @@ public class PostService {
     private final PostMapper postMapper;
 
     @Autowired
-    public PostService(PostRepository postRepository, PostMapper postMapper)
-    {
+    public PostService(PostRepository postRepository, PostMapper postMapper) {
         this.postMapper = postMapper;
-        this.postRepository =postRepository;
+        this.postRepository = postRepository;
     }
 
-    public List<Post>getAllPosts()
-    {
+    public List<Post> getAllPosts() {
         List<Post> posts = new ArrayList<>();
         postRepository.findAll().iterator().forEachRemaining(posts::add);
         return posts;
     }
 
+    public Optional<Post> getPost(Long id) {
+        Optional<Post> optionalPost = postRepository.findById(id);
+        return optionalPost;
+    }
+
+    public void deletePost(Long id) {
+        Optional<Post> post = postRepository.findById(id);
+        if (post.isPresent()) {
+            postRepository.delete(post.get());
+        }
+    }
+
+    public Post updatePost(Long id, Post postUpdated) {
+        Optional<Post> postOptional = postRepository.findById(id);
+
+        if (postOptional.isPresent()) {
+            postUpdated.setId(id);
+            return postRepository.save(postUpdated);
+
+        }
+        return postUpdated;
+    }
     public PostDto savePost(PostDto postDto)
     {
         Post post = postMapper.mapPostDtoToPost(postDto);
         Post savedPost = postRepository.save(post);
         return postMapper.mapPostToPostDto(savedPost);
-
     }
+
 }
