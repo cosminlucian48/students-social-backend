186fc9f02ea8fefe30b4f0659f2a77fa3dcaa802
petrino <petrino.popa@yahoo.com>
2022-01-07 13:19:07 +0100

    mail

---
+++ src/main/java/mail/EmailService.java
@@ -0,0 +1,14 @@
+package mail;
+
+public interface EmailService {
+    void sendSimpleMessage(String to,
+                           String subject,
+                           String text);
+    void sendSimpleMessageUsingTemplate(String to,
+                                        String subject,
+                                        String ...templateModel);
+    void sendMessageWithAttachment(String to,
+                                   String subject,
+                                   String text,
+                                   String pathToAttachment);
+}
---
+++ src/main/java/mail/JavaMailSender.java
@@ -0,0 +1,9 @@
+package mail;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+public interface JavaMailSender {
+
+
+}
